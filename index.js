60d01b7822ae8cf15446cb45330cd122976bcb1f
ji sun lee <ellijs@hotmail.com>
2021-03-23 19:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function updateObjectWithKeyAndValue(object, key, value) {
+<<<<<<< HEAD
   return Object.assign({}, object, {[key]: value});
+=======
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
+>>>>>>> 4f75f9df319ce03c19ba4ab84b0a3f91eeba6125
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
@@ -8,8 +14,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
+<<<<<<< HEAD
   return delete newObj[key];
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   return delete object[key];
+=======
+  delete newObj.key;
+  return newObj;
+>>>>>>> 4f75f9df319ce03c19ba4ab84b0a3f91eeba6125
 }
